20d21ae4073b6fb2db00ba07bafc161bf2294623
Pelumi Honey <pelumioni25@gmail.com>
2022-08-23 12:51:04 +0100

    blog ui updates

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="z-50">
 		<nav class="w-full m-auto bg-[#302f3f] shadow-default px-20px pt-60px pb-20px fixed left-[50%] -translate-x-1/2 translate-y-0 desktop:pt-50px desktop:pb-12px">
 			<section class="fixed top-0 left-0 bg-primary-100 w-full h-40px py-8px px-12px flex items-center justify-center font-medium text-12 text-white-100 desktop:text-14">
 				<span>Give us a star on GitHub</span>
